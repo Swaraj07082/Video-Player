f3e1350519e9ba59efbb37214d03f9a410d0d1fd
Swaraj <swarajmali072004@gmail.com>
2023-10-31 21:42:40 +0530

    Initial Commit

---
+++ src/Components/RButton.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function RButton() {
+  return (
+<>
+<div class="container text-center" style={{ paddingTop : 150}}>
+  <div class="row">
+    <div class="col" style={{ paddingLeft:250}}>
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+  <label class="form-check-label" for="defaultCheck1" style={{ color : 'white'}}>
+    Deer
+  </label>
+</div>
+    </div>
+    <div class="col">
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+  <label class="form-check-label" for="defaultCheck1" style={{ color : 'white'}}>
+    Snail
+  </label>
+</div>
+    </div>
+    <div class="col">
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+  <label class="form-check-label" for="defaultCheck1" style={{ color : 'white'}}>
+    Cat
+  </label>
+</div>
+    </div>
+    <div class="col" style={{paddingRight : 250}}>
+    <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1"/>
+  <label class="form-check-label" for="defaultCheck1" style={{ color : 'white'}}>
+    Spider
+  </label>
+</div>
+    </div>
+  </div>
+</div>
+
+
+
+</>
+
+  )
+}
